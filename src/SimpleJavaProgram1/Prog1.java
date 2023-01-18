2975c9eb628108652a1587d427af849e0e7f6fbe
Ashwini Devkate <ashwinidevkate15@gmail.com>
2023-01-18 12:19:15 +0530

    first

---
+++ src/SimpleJavaProgram1/Prog1.java
@@ -0,0 +1,11 @@
+package SimpleJavaProgram1;
+
+public class Prog1 {
+
+	public static void main(String[] args) {
+		System.out.println("Hi,Good morning ");
+		// TODO Auto-generated method stub
+
+	}
+
+}
